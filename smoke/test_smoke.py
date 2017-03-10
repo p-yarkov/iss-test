a1e361c89adde08ba6b834e755888cd91d904fad
p-yarkov <nailztf@gmail.com>
2017-03-10 11:48:41 +0300

    Тест 4 проверки по шагам кроме проверки показа видео

--- smoke/test_smoke.py
+++ smoke/test_smoke.py
@@ -13,15 +13,20 @@ def test_smoke_object(securos_auto):
 
     cli = securos_auto["client"].top_window()
     cli["CheckBox2"].click_input()
+
     tree = securos_auto["core"].top_window()
-    menu = securos_auto["core"].Menu
+    assert tree.exists() # Шаг 1 - Ожидаемый результат
     tree["Система"].click_input(double=True)
     tree["SecurOS Enterprise"].click_input(double=True)
     tree["Оборудование"].click_input(double=True)
-    tree.window(title_re="Компьютер*").click_input(button="right")
-    menu["Создать"].click_input()
+    tree.window(title_re="Компьютер*").click_input()
+    assert tree.window(title_re="Компьютер*").is_selected() # Шаг 2 - Ожидаемый результат
+    securos_auto["core"]["Pane22"]["Создать"].click_input()
+    menu = securos_auto["core"].Menu
+    assert menu.exists() # Шаг 3 - Ожидаемый результат
     menu["Устройство видеозахвата"].click_input()
     sets = securos_auto["core"].window(title_re="Параметры")
+    assert sets.exists() # Шаг 4 - Ожидаемый результат
     sets["ComboBox"].click_input()
     sets["ListBox"].type_keys("{UP 1}{DOWN 40}{ENTER}")
     sets["Ok Enter"].click_input()
@@ -29,11 +34,10 @@ def test_smoke_object(securos_auto):
     pane["ComboBox33"].click_input()
     pane["01"].click_input()
     pane["ОК"].click_input()
+    assert tree["Устройство видеозахвата 1"].exists() # Шаг 5 - Ожидаемый результат
     tree["Устройство видеозахвата 1"].click_input(button="right")
     menu["Создать"].click_input()
     menu["Камера"].click_input()
     sets["Ok Enter"].click_input()
     pane["ОК"].click_input()
-
-#    assert tree.window(title="Камера 1").exists()
-#   assert tree.window(title="Камера 2").exists()
+    assert tree.window(title="Камера 1 [1]").exists() # Шаг 6 - Ожидаемый результат
