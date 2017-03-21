4c31a08ce7384101088385de4444de1b55c3cea1
p-yarkov <nailztf@gmail.com>
2017-03-21 14:21:17 +0300

    Сделан Тест № 2 с правками фикстур под визард

--- smoke/conftest.py
+++ smoke/conftest.py
@@ -22,12 +22,13 @@ def securos_pids():
     securos_pids = {
         "securos.exe": 0,
         "client.exe": 0,
-        "monitor.exe": 0
+        "monitor.exe": 0,
+        "wizard.exe": 0
     }
     clipid = []
     mem = []
 
-    PROCS = ["securos.exe", "client.exe", "monitor.exe"]
+    PROCS = ["securos.exe", "client.exe", "monitor.exe", "wizard.exe"]
 
     for proc in PROCS: #Ищем PIDы
         for tpid in psutil.process_iter():
@@ -36,10 +37,11 @@ def securos_pids():
             elif tpid.name() == proc and proc is "client.exe": # client.exe два процесса, нам нужен 1
                 clipid.append(tpid.pid)
 
-    for pid in clipid: # Выбираем тот процесс, который больше памяти занял
-        pr = psutil.Process(pid)
-        mem.append(pr.memory_info().rss)
-    securos_pids["client.exe"] = clipid[mem.index(max(mem))]
+    if clipid:
+        for pid in clipid: # Выбираем тот процесс, который больше памяти занял
+            pr = psutil.Process(pid)
+            mem.append(pr.memory_info().rss)
+        securos_pids["client.exe"] = clipid[mem.index(max(mem))]
 
     return securos_pids
 
@@ -51,29 +53,21 @@ def securos_auto(securos_pids):
     for pid in securos_pids.values():
         assert psutil.pid_exists(pid)
 
-    return {
-        "core": Application(backend="uia").connect(process=securos_pids["securos.exe"]),
-        "client": Application(backend="uia").connect(process=securos_pids["client.exe"]),
-        "monitor": Application(backend="uia").connect(process=securos_pids["monitor.exe"])
-    }
+    if securos_pids["wizard.exe"] == 0:
+        return {
+            "core": Application(backend="uia").connect(process=securos_pids["securos.exe"]),
+            "client": Application(backend="uia").connect(process=securos_pids["client.exe"]),
+            "monitor": Application(backend="uia").connect(process=securos_pids["monitor.exe"])
+        }
+    else:
+        return {
+            "wizard": Application(backend="uia").connect(process=securos_pids["wizard.exe"])
+        }
 
 
 @pytest.fixture(scope="function")
 def securos_start():
     '''Запускаем SecurOS'''
 
-    PROCS = ["securos.exe", "client.exe", "monitor.exe"]
-
     psutil.Popen(os.path.join(pytest.SECUROS_WIN, "securos.exe")) # Ждем запуска всех процессов
-    pid = 0
-    t = 0
-    while pid < 3:
-        pid = 0
-        for proc in PROCS:
-            for tpid in psutil.process_iter():
-                if tpid.name() == proc:
-                    pid+=1
-        time.sleep(1)
-        t+=1
-        if t >= 10:
-            assert False
+    time.sleep(10)
